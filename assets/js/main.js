8d97fcd878a07f4fee2743e4f8e91507bcae9e08
Marc-André Charpentier <emma.charp@me.com>
2020-07-31 07:52:22 -0400

    First Dump

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+let tan = Math.tan(Math.PI/5);
+
+var ready = (callback) => {
+  if (document.readyState != "loading") callback();
+  else document.addEventListener("DOMContentLoaded", callback);
+}
+
+ready(() => {
+
+	document.getElementsByTagName('aside')[0].setAttribute('style', '--m:5; --tan:'+tan.toFixed(2))+';'
+});
