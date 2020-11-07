e91c124d867177b33c06691c729c11b5d5f09ddf
Marc-André Charpentier <emma.charp@me.com>
2020-11-07 17:08:50 -0500

    Ménage et Organisation ; Parallax

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -1,64 +1,126 @@
 (function () {
 	'use strict';
 
-	// Snap to CSS Backgorund Grid
+// Necessary DOM Elements
 
-	const html = document.getElementsByTagName('html')[0];
+	const html = document.querySelector('html');
 	const doc = document.documentElement;
+	const sections = document.querySelectorAll('main>section');
+	const anchors = document.querySelectorAll('nav>a');
+	const parallaxItems = document.querySelectorAll('[data-parallax]');
 
-	const ComputeRowRest = () => {
+
+// Snap to CSS Grid Paper Pattern Function
+
+	const setRowRest = () => {
 		let Row = parseFloat(window.getComputedStyle(html).getPropertyValue('line-height'));
 		let ViewportWidth = doc.clientWidth;
 		let ViewportHeight = doc.clientHeight;
 		let H_RowRest = ViewportWidth - (Math.floor(ViewportWidth / Row) * Row);
 		let V_RowRest = ViewportHeight - (Math.floor(ViewportHeight / Row) * Row);
-		document.body.style.cssText = '--H_RowRest: '+H_RowRest+'px; --V_RowRest:'+V_RowRest+'px;';
+
+		document.body.style.cssText = `--H_RowRest: ${H_RowRest}px; --V_RowRest:${V_RowRest}px;`;
 	}
-	
-	window.addEventListener('DOMContentLoaded', ComputeRowRest);
-	window.addEventListener('resize', ComputeRowRest);
 
 
-
+// Internal Navigation Behavior Function
 
 	const onAnchorClick = (e) => {
 		const index = [...anchors].indexOf(e.target);
 
-		document.querySelector("main").scrollTo({
+		document.querySelector('main').scrollTo({
 			left: index * window.innerWidth,
 			top: index * window.innerHeight
 		});
 	}
 
-	// Set Elements
-	const sections = document.querySelectorAll("main>section");
-	const anchors = document.querySelectorAll("nav>a");
 
-	// Init Observer
-	const threshold = 0.5;
-	const observer = new IntersectionObserver((entries, observer) => {
-		entries.forEach((entry) => {
+// Sections Observer with Binding to Internal Navigation Anchors Function
+
+	const onSectionObserved = (items, observer) => {
+		items.forEach((entry) => {
 			const { intersectionRatio, target } = entry;
 			const sectionIndex = [...sections].indexOf(target);
-
-			if (intersectionRatio >= threshold) {
-				target.classList.add("is-visible");
-				if (anchors.length) anchors[sectionIndex].classList.add("is-visible");
+			console.log(target, items, observer);
+			if (intersectionRatio >= observer.thresholds[0]) {
+				target.classList.add('is-visible');
+				if (anchors.length) anchors[sectionIndex].classList.add('is-visible');
 			} else {
-				target.classList.remove("is-visible");
-				if (anchors.length) anchors[sectionIndex].classList.remove("is-visible");
+				target.classList.remove('is-visible');
+				if (anchors.length) anchors[sectionIndex].classList.remove('is-visible');
 			}
 		});
-	}, {
-		threshold
-	});
-	sections.forEach((el, index) => {
-		observer.observe(el);
-	});
+	}
 
-	// Nav Events
-	anchors.forEach((el) => {
-		el.addEventListener("click", onAnchorClick);
+
+// CSS Shapes Parallax Functions
+
+	 const onParallaxObserved = (items) => {
+		items.forEach((item) => {
+			const i = item;
+
+			if (i.isIntersecting) {
+				i.target.classList.add('is-visible');
+				setParallaxData();
+			} else {
+				i.target.classList.remove('is-visible');
+			}
+		});
+	 }
+
+	const setParallaxData = () => {
+		[...parallaxItems].filter(el => el.classList.contains('is-visible')).forEach((el) => {
+			const e = el;
+			const eH = e.offsetHeight;
+			const wH = window.innerHeight;
+			const curScroll = (e.getBoundingClientRect().top - wH) * -1;
+			const toScroll = eH + wH;
+			let progress = Math.min(Math.max(curScroll, 0) / toScroll, 1);
+			const factor = parseFloat(e.getAttribute('data-factor')) || 0.2;
+			const fit = e.getAttribute('data-fit') || 'contain';
+
+			if (fit === 'contain') {
+				progress = (progress < 0.5 ? 1 - progress * 2 : progress * -2 + 1) * -1;
+			} else {
+				progress = 1 - progress;
+			}
+
+			e.style.setProperty('--scrollParallax', progress * (eH * factor));
+		});
+	 }
+
+
+// Init All
+
+	window.addEventListener('DOMContentLoaded', function() {
+		// Set RowRest
+		setRowRest();
+		window.addEventListener('resize', setRowRest);
+
+		// Sections Observer
+		const sectionObserver = new IntersectionObserver(onSectionObserved, {
+			threshold: 0.5,
+		});
+		sections.forEach((item, index) => {
+			sectionObserver.observe(item);
+		});
+
+		// Nav Events
+		anchors.forEach((item) => {
+			item.addEventListener('click', onAnchorClick);
+		});
+
+		// Parallax Observer
+		const parallaxObserver = new IntersectionObserver(onParallaxObserved);
+		parallaxItems.forEach((el) => {
+			parallaxObserver.observe(el);
+		});
+
+		// Parallax Event Handler
+		setParallaxData();
+		const parallaxEventHandler = () => requestAnimationFrame(setParallaxData);
+		window.addEventListener('resize', parallaxEventHandler);
+		window.addEventListener('scroll', parallaxEventHandler);
 	});
 
 })();
