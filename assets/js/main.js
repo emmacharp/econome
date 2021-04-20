6b0ac8c8bcdba17a2fd812a9df495a02c7006039
emmacharp <emmacharp@users.noreply.github.com>
2021-04-20 11:35:55 +0000

    deploy: 6d19b5183bdd79e9904a4a8d45b8597d305c7cff

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,6 @@
 
 	const sections = document.querySelectorAll('main>section');
 	const anchors = document.querySelectorAll('nav>a');
-	const parallaxItems = document.querySelectorAll('[data-parallax]');
 	const wikiLinks = document.querySelectorAll('[data-wiki]');
 	const wikiViewer = document.querySelector('#wikiViewer');
 	const wikiDump = document.querySelector('#wikiDump');
@@ -51,42 +50,6 @@
 		});
 	}
 
-
-// CSS Shapes Parallax Functions
-
-	 const onParallaxObserved = (items) => {
-		items.forEach((item) => {
-			const i = item;
-
-			if (i.isIntersecting) {
-				i.target.classList.add('is-visible');
-				setParallaxData();
-			} else {
-				i.target.classList.remove('is-visible');
-			}
-		});
-	 }
-
-	const setParallaxData = () => {
-		[...parallaxItems].filter(el => el.classList.contains('is-visible')).forEach((el) => {
-			const e = el;
-			const eH = e.offsetHeight || e.getBBox().height;
-			const wH = window.innerHeight;
-			const curScroll = (e.getBoundingClientRect().top - wH) * -1;
-			const toScroll = eH + wH;
-			let progress = Math.min(Math.max(curScroll, 0) / toScroll, 1);
-			const factor = parseFloat(e.getAttribute('data-factor')) || 0.2;
-			const fit = e.getAttribute('data-fit') || 'contain';
-
-			if (fit === 'contain') {
-				progress = (progress < 0.5 ? 1 - progress * 2 : progress * -2 + 1) * -1;
-			} else {
-				progress = 1 - progress;
-			}
-
-			e.style.cssText = `--scrollParallax: ${progress * (eH * factor)}; --scrollFactor: ${progress};`;
-		});
-	 }
 
 
  // Fetching content from Wikipedia API
@@ -172,15 +135,7 @@
 			item.addEventListener('click', onAnchorClick);
 		});
 
-		// Parallax Observer
-		const parallaxObserver = new IntersectionObserver(onParallaxObserved);
-		parallaxItems.forEach((el) => {
-			parallaxObserver.observe(el);
-		});
-
-		// Parallax Event Handler
-		const parallaxEventHandler = () => requestAnimationFrame(setParallaxData);
-		const scrollInterval = setInterval(parallaxEventHandler, 10);
+		
 	});
 
 })();
---
+++ assets/js/parallax.js
@@ -0,0 +1,56 @@
+const parallaxItems = document.querySelectorAll('[data-parallax]');
+const setParallaxData = () => {
+	[...parallaxItems].forEach((el) => {
+		const e = el;
+		const eH = e.offsetHeight || e.getBBox().height;
+		const wH = window.innerHeight;
+		const curScroll = (e.getBoundingClientRect().top - wH) * -1;
+		const toScroll = eH + wH;
+		let progress = Math.min(Math.max(curScroll, 0) / toScroll, 1);
+		const factor = parseFloat(e.getAttribute('data-factor')) || 0.2;
+		const fit = e.getAttribute('data-fit') || 'contain';
+		if (fit === 'contain') {
+			progress = (progress < 0.5 ? 1 - progress * 2 : progress * -2 + 1) * -1;
+		} else {
+			progress = 1 - progress;
+		}
+
+		e.style.cssText = `--scrollParallax: ${progress * (eH * factor)}; --scrollFactor: ${progress};`;
+	});
+}
+
+(function () {
+	'use strict';
+
+	// CSS Shapes Parallax Functions
+
+	const onParallaxObserved = (items) => {
+		items.forEach((item) => {
+			const i = item;
+
+			if (i.isIntersecting) {
+				i.target.classList.add('is-visible');
+				setParallaxData();
+			} else {
+				i.target.classList.remove('is-visible');
+			}
+		});
+	}
+
+
+	window.addEventListener('DOMContentLoaded', function() {
+		// Parallax Observer
+		// const parallaxObserver = new IntersectionObserver(onParallaxObserved);
+		// parallaxItems.forEach((el) => {
+		// 	parallaxObserver.observe(el);
+		// });
+		const parallaxItems = document.querySelectorAll('[data-parallax]');
+		// Parallax Event Handler
+		const parallaxEventHandler = () => requestAnimationFrame(setParallaxData);
+		setParallaxData();
+		window.addEventListener('resize', parallaxEventHandler);
+		window.addEventListener('scroll', parallaxEventHandler);
+		// const scrollInterval = setInterval(parallaxEventHandler, 10);
+
+	});
+})();
